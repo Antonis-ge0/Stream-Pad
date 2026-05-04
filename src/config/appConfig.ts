57ca1ffb5a8b1b242469aac19210b1c05129a50f
Antonis Geo <antgeoath@csd.auth.gr>
2026-05-04 23:45:39 +0300

    Test

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   name: "Stream Deck",
-  version: "1.0.0",
+  version: "0.1.1",
   feedbackFormUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSeNMxf6YHH9jKiOnXKVtkDlyra7X9zx19LP-UiTwLGW3kAkkA/viewform",
   maxSoundDurationSeconds: 30,
