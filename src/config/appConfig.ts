a4874c88be4db5440e07e3dbcfa991c5cdeeb069
Antonis Geo <antgeoath@csd.auth.gr>
2026-05-11 18:12:21 +0300

    Remove outer app logo outline

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   name: "Stream Pad",
-  version: "0.1.27",
+  version: "0.1.28",
   feedbackFormUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSeNMxf6YHH9jKiOnXKVtkDlyra7X9zx19LP-UiTwLGW3kAkkA/viewform",
   maxSoundDurationSeconds: 30,
